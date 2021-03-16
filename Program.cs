e7b97f321dcdfb17c83a108faf8a349525fb30b8
Jacek Sota <s20423@pjwstk.edu.pl>
2021-03-16 21:59:50 +0100

    Dodano pliki z foldera

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace apbd_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interpolacja stringa
+           
+
+            Student stud = new Student {
+                //crtl_space
+                FirstName = "Jac",
+                _myVar = 52
+            };
+
+            Console.WriteLine($"Imie studenta to: {stud.FirstName} i ma {stud._myVar}");
+
+            var tmp = "AVC";
+
+            //ArrayList -> depricated
+            var list = new List<string> {"Ala", "ma", "kota"};
+
+                //foreach(var wrt :string in list){
+                //Console.WriteLine(wrt);
+                //}
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apbd_01
+{
+    class Student
+    {
+        //public TYPE type { get; set; }
+        //bool 
+        public string FirstName { get; set; } = "a";
+
+        public int _myVar;
+        //private int MyVar {
+            //get { return _myVar};
+            //set { _myVar = value};
+        //}
+    }
+}
